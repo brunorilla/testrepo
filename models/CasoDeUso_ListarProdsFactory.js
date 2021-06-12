510b4739ec0d215ea16fcbe67dcda3220eea7242
Bruno Rilla <brilla@Lenovo.com>
2021-06-12 20:09:17 -0300

    starting project again

--- models/CasoDeUso_ListarProdsFactory.js
+++
@@ -1,7 +0,0 @@
-const CasoDeUso_ListarProds = require('./CasoDeUso_ListarProds.js')
-
-function CasoDeUso_ListarProdsFactory(listarFunc){
-    return new CasoDeUso_ListarProds(listarFunc);
-}
-
-module.exports = CasoDeUso_ListarProdsFactory;
